ca0510d0713400c131d7744e2b2c6d07e6c8d29c
leifkreutzheide <leifkreutzheide@gmail.com>
2025-12-19 15:06:59 +0100

    Normalize key input for character shifting
    Added normalization for the key input to ensure it stays within the range of 0-25.

--- src/seminar4/Task_4_3.c
+++ src/seminar4/Task_4_3.c
@@ -2,11 +2,16 @@
 #include <ctype.h>
 
  int main() { 
-    int c, key;
+    int c, key, normalized;
     printf("Enter Key: ");
     scanf("%d", &key);
+    
+
+    // normalises a given range 
+
+    normalized = ((key % 26) + 26) % 26;
 
     while ( (c = getchar()) != EOF)
-   
-    putchar(tolower(c + key));
+    putchar(tolower(c +normalized));
  }
+
