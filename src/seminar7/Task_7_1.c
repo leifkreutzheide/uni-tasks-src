75e785cea6e5a9de1a37051ba66f129b8844d44e
leifkreutzheide <leifkreutzheide@gmail.com>
2026-01-08 21:08:55 +0100

    Update Task_7_1 and Task_7_2

--- src/seminar7/Task_7_1.c
+++ src/seminar7/Task_7_1.c
@@ -1,7 +1,17 @@
 // task: What does the call "a.out this is only a test" accomplish? Interpret the entire output!
 /* answer: 
 
+The command runs the program a.out with 5 arguments:
 
+argc = 6 (program name + 5 words)
+argv[0] = "a.out"
+argv[1] = "this"
+argv[2] = "is"
+argv[3] = "only"
+argv[4] = "a"
+argv[5] = "test"
+
+The command line splits the input by spaces. So the program receives each word as a separate string in the argv array.
 
 */
 
---
+++ src/seminar7/Task_7_2.c
@@ -0,0 +1,29 @@
+// Task: Task 7.2: Test the perror() error message assuming that the file to be opened is not readable
+// or writeable. (Change with the Unix command chmod.)
+
+// solution: 
+
+
+#include <stdio.h>12 #include <stdlib.h>13
+int main( int argc, char *argv[] ) {
+
+     int c;
+     FILE *source, *target, *eopen();
+     source = eopen(argv[1], "r");
+     target = eopen(argv[2], "w");
+
+     while ( (c = getc(source)) != EOF ) /* do this: */ putc(c, target);
+    
+    fclose(source);
+    fclose(target);
+ }
+
+ /* opening File with error handling */
+ FILE *eopen(char *name, char *mode) {
+ FILE *fp;
+ if ( (fp = fopen(name,mode)) == NULL) {
+        perror(name); /* Library function delivers error diagnosis */
+        exit(1);
+    }
+         return fp;
+    }
